409d4355bf07f4d743217a0228383cb7eccebc2e
Michael Ushakov <um.nix.user@gmail.com>
2018-08-13 13:37:56 +0300

    some survey in measurer.h

--- src/measurer.cpp
+++ src/measurer.cpp
@@ -2,8 +2,21 @@
 
 namespace Performance
 {
-	/*Measurer :: Measurer()
+	Performance::Measurer::Measurer()
+		:_state(0), _result(0)
 	{
 
-	}*/
+	}
+
+	// todo: umv: apply conditional (os specifiv impl)
+	void Performance::Measurer::Start()
+	{
+		
+	}
+
+	// todo: umv: apply conditional (os specifiv impl)
+	void Performance::Measurer::Stop()
+	{
+
+	}
 }
--- src/measurer.h
+++ src/measurer.h
@@ -1,12 +1,40 @@
 #ifndef SRC_MEASURER_H
 #define SRC_MEASURER_H
 
-namespace Perfomance
+namespace Performance
 {
+	/// ####################################################################### TIME MEASURE SURVEY SUMMARY #############################################################################
+	/// Windows:
+	///     1) Low precision WIN API functions GetTickCount(), resolution is ~ 10 ms
+	///     2) High precision WIN API function QueryPerformanceCounter
+	/// Linux:
+	///     1) Use function clock_gettime(3) with CLOCK_MONOTONIC_RAW from Linux API, time.h (resolution is ~ ns)
+	/// Cross platform: 
+	///     1) ring-3 instruction: _rdtsc (~10-20 ns) 
+	///        RDTSC multicore accuracy:
+	///           The time stamp counter in newer processors may support an enhancement, referred to as invariant TSC.Processor's support for invariant TSC is indicated 
+	///           by CPUID.80000007H:EDX[8]. The invariant TSC will run at a constant rate in all ACPI P - , C - . and T - states.This is the architectural behavior moving 
+	///           forward.On processors with invariant TSC support, the OS may use the TSC for wall clock timer services (instead of ACPI or HPET timers).
+	///           TSC reads are much more efficient and do not incur the overhead associated with a ring transition or access to a platform resource."
+	///           In Linux see: __cpuinit int unsynchronized_tsc(void) to check for invariant TSC (Time Stamp Counter)
+	///     2) HPET (High Performance Event Timer), available on motherboards from 2006+, Higher precision than RTC (RDTSC)
+	///        HPET is NOT supported by Windows XP sp1, Windows 2003 and old linux
+	///        HPET IS supported by:
+	///            Windows Vista, Windows 2008, Windows 7, Windows 8, Windows 10
+	///	           Mac OS X(версия для x86)
+	///	           Linux версий 2.6 (драйвер rtc - cmos вместо rtc)
+	///	           FreeBSD
+	///	           eComStation(OS / 2) c неофициальным ядром OS / 4 начиная с версии svn 4502
+	///	           KolibriOS
+	/// REMARKS:
+	///    All measure could be in accurate due to a lot of factors (CPU cache usage, dynamic frequency changes (i.e. Turbo Boost), energy saving mode, e.t.c.)
+	/// ###################################################################################################################################################################################
 	class Measurer
 	{
 	public:
 		Measurer();
+		// delete copy constructor, des
+		//~Measurer() delete
 		void Start();
 		void Stop();
 		inline bool IsRunning() { return _state; }
